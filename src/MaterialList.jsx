d31cc9533b98b6ff19200d0d418a9e5ba1dc8d24
Dec Keighley <declan@smudge.com>
2021-11-19 10:05:14 +1300

    Removed unused imports.

--- src/MaterialList.jsx
+++ src/MaterialList.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import materials from './materials.json'
-import { Box, Card, CardHeader, Grid } from '@material-ui/core';
-import { MaterialCard } from './components/materialCard';
-import { OrderSummary } from './components/orderSummary';
 
 export default function MaterialList() {
   // Utilities
